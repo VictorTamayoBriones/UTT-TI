327d599a0b08be3f55c9bb57a926e0de403f2da2
Víctor Tamayo <victortamayo509@gmail.com>
2022-09-06 08:40:04 -0500

    logout btn

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router-dom"
+import { PrivateRoutes, PublicRoutes } from "../../models";
 import { Nav } from "./styled-components/Nav"
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
   return (
     <Nav>
-        <h3>User Name</h3>
-        <ul>
-            <li><button>Logout</button></li>
-        </ul>
+      <h3>User Name</h3>
+      <ul>
+          <li><button onClick={()=>navigate(PublicRoutes.LOGIN)} >Logout</button></li>
+      </ul>
     </Nav>
   )
 }
